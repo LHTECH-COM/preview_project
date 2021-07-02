508e15bee1304b4e87f275602cdf831a982e8635
Jessica Tran <jessica.loan.tran@gmail.com>
2021-07-02 15:59:46 +0700

    update code

--- test2_project/accounts.py
+++ test2_project/accounts.py
@@ -1,58 +1,50 @@
-import json
 import requests
-
-COMPANY_NAME_TYPE = 1
-USER_NAME_TYPE = 2
-NAME_TYPE = 3
+import json
 
 API_URL = "https://jsonplaceholder.typicode.com/users"
+
+COMPANY_TYPE = 1
+NAME_TYPE = 2
+USERNAME_TYPE = 3
+
 class Accounts(object):
     """
-        A class is information of account
+        A class is information of user
+        
         
         Attributes
-        --------------------------------
-        accounts: array of string return from API
-        
+        -------------------------
+        accounts: array of str
+            list of user read from api
+            
+            
+        Methods
+        ------------------------
+        get_api_account_list(): object
+            get data of user from api
+        search_by_value(): object
+            return list of user search by name or username or company name
+        search_by_company(): object
+            return list of user search by company name
+        search_by_name(): object
+            return list of user search by name
+        search_by_user_name(): object
+            return list of user search by user name
     """
     
     def __init__(self):
         self.accounts = []
-               
-    
-    def search_data(self, search_value="", search_type = ""):
-        """
-            search data from rest api
-            
-            Arguments:
-                search_value: value want to search
-                search_type: type of search: company, name, username
-        """
         
-        data_list = []
+    def get_api_account_list(self):
+        """
+            get list of data from api
+        """
         try:
             response = requests.get(API_URL)
             if response.status_code == 200:
                 response_data = response.json()
                 for data in response_data:
-                    try:
-                        if search_type == COMPANY_NAME_TYPE: #search by company name
-                            if data["company"]["name"] == search_value:
-                                data_list.append(data)
-                                continue
-                        if search_type == USER_NAME_TYPE:
-                            if data["username"] == search_value:
-                                data_list.append(data)
-                                continue
-                        if search_type == NAME_TYPE:
-                            if data["name"] == search_value:
-                                data_list.append(data)
-                                continue
-                    except KeyError as key_rr:
-                        pass
-               
-                return data_list
-                
+                    self.accounts.append(data)
             else:
                 response.raise_for_status()
         except requests.exceptions.HTTPError as http_err:
@@ -63,48 +55,63 @@ class Accounts(object):
             print (tim_err)
         except requests.exceptions.RequestException as req_err:
             print (req_err)
-            
-    def search_by_company(self, search_value):
-        """
-            search by company
-            arguments:
-                search_value: value want to search
-        """
-        data = self.search_data(search_value, COMPANY_NAME_TYPE)
-        if data:
-            print(data)
-        else:
-            print ("not found data")
-            
-    def search_by_username(self, search_value):
-        """
-            search by user name
-            arguments:
-                search_value: value want to search
-        """
-        data = self.search_data(search_value, USER_NAME_TYPE)
-        if data:
-            print(data)
-        else:
-            print ("not found data")
-            
-    def search_by_name(self, search_value):
-        """
-            search by name
-            arguments:
-                search_value: value want to search
-        """
-        data = self.search_data(search_value, NAME_TYPE)
         
-        if data:
-            print(data)
+    def search_by_value(self, search_value, search_type):
+        """
+            filter list of data by search type and search value
+            
+            Arguments:
+                search_value: value want to search
+                search_type: type of search
+        """
+        #list of array data
+        data_filter = []
+        for data in self.accounts:
+            try:
+                if search_type == COMPANY_TYPE:
+                    if data["company"]["name"] == search_value:
+                        data_filter.append(data)
+                        continue
+                if search_type == NAME_TYPE:
+                    if data["name"].startswith(search_value):
+                        data_filter.append(data)
+                        continue
+                if search_type == USERNAME_TYPE:
+                    if data["username"] == search_value:
+                        data_filter.append(data)
+                        continue
+                
+            except KeyError as key_err:
+                pass
+            
+        return data_filter
+    
+    def search_by_company(self, search_value = ""):
+        data_return = self.search_by_value(search_value, COMPANY_TYPE)
+        if data_return:
+            print (data_return)
         else:
-            print ("not found data")
+            print ("data not found")
+            
+    def search_by_name(self, search_value = ""):
+        data_return = self.search_by_value(search_value, NAME_TYPE)
+        if data_return:
+            print (data_return)
+        else:
+            print ("data not found")
+            
+    def search_by_user_name(self, search_value = ""):
+        data_return = self.search_by_value(search_value, USERNAME_TYPE)
+        if data_return:
+            print (data_return)
+        else:
+            print ("data not found")
         
 ac = Accounts()
-
-ac.search_by_company(search_value = "Romaguera-Crona")
-ac.search_by_name(search_value = "Ervin Howell")
-ac.search_by_username(search_value = "Karianne")
-
-
+ac.get_api_account_list()
+print ("search by company:")
+ac.search_by_company("Yost and Sons")
+print ("search by name start with C")
+ac.search_by_name(search_value = "C")
+print("search by username")
+ac.search_by_user_name("Moriah.Stanton")
--- test2_project/data.py
+++ test2_project/data.py
@@ -1,92 +1,115 @@
 import requests
-import json
 import csv
+import json
 
 API_URL = "https://httpbin.org/base64/"
 
 
 class Data(object):
     """
-         A class save string data with base64
-         
-         Attributes
-         ------------------
-         data_list: array of string
-              
+        A class to read data from csv file
+        
+        Attributes
+        ---------------------
+        data_list: array of string
+            data read from csv file
+            
+            
         Methods
-        ------------------------
+        -------------------------
         read_data_from_csv_file(): void
-            read data from csv file
-        get_list_data(): object
-            return list of data read from csv file
-        decode_base64(): 
-        return data decode with base64 from api
+            read all data from csv file
+        decode_data():
+            return value after decode from api
+        get_list_data():
+            return list of data read from csv file as json format
+        write_data_to_csv_file(): void
+            write data decode successful to csv file
     """
-    #array of string read from csv file
     
     def __init__(self):
         self.data_list = []
-    
+        self.data_decode_list = []
+        
+    def decode_data(self, value):
+        """
+            decode data with base64
+            arguments:
+                value: value want to decode
+        """
+        response_data = ""
+        if value:
+            try:
+                api_path = f'{API_URL}{value}'
+                response = requests.get(api_path)
+                if response.status_code == 200:
+                    response_data = response.text
+                    
+                else:
+                    response.raise_for_status()
+            except requests.HTTPError as http_err:
+                print (http_err)
+            except requests.ConnectionError as conn_err:
+                print (conn_err)
+            except requests.Timeout as tim_err:
+                print (tim_err)
+            except requests.RequestException as req_err:
+                print (req_err)
+        return response_data
         
     def read_data_from_csv_file(self, file_name):
         """
             read data from csv file
-            Arguments:
+            arguments:
                 file_name: path and file name of csv file
+            
         """
         try:
+            print("begin read data from csv file")
             with open(file_name, "r") as file:
                 reader = csv.reader(file, delimiter=",")
                 for index, row in enumerate(reader):
                     self.data_list.append(row[0])
-                     
-            print("Read data from csv file sucessfully")
-        except IOError as io_err:
-            print (io_err)
-            print("read data from csv file fail")
+                    self.data_decode_list.append(self.decode_data(row[0]))
             
+            print ("read data to csv file successfully")
+        except EOFError as err:
+            print ("read data from csv file fail")
             
     def get_list_data(self):
         """
-            list data read from csv file as json format
+            list of data read from csv file and decode with base64 from api
         """
+        return [{
+                "decode_value": da
+            } for da in self.data_decode_list]
+            
+    def write_data_to_csv_file(self):
+        """
+            write data decode successful to csv file
+        """
+        data_write = []
+        data_list = self.data_decode_list
+        for data in data_list:
+            if "Incorrect Base64 data try" not in data:
+                data_write.append(data)
         
-        return json.dumps([{
-            "row_data": self.decode_base64(row)
-            } for row in self.data_list])
-    
-    def decode_base64(self, value):
-        """
-            decode data with base64
-        """
+        
         try:
-            api_link = f'{API_URL}{value}'
+            with open("data_new.csv", mode="w") as csv_file:
+                writer = csv.writer(csv_file, delimiter=",")
+                for da in data_write:
+                    writer.writerow([da])
+                    
+            print ("write data to csv file successfully")
             
-            response = requests.get(api_link)
-            if response.status_code == 200:
-                response_data = response.text
-                return response_data
-            else:
-                response.raise_for_status()
-        except requests.exceptions.HTTPError as http_err:
-            print (http_err)
-        except requests.exceptions.ConnectionError as conn_err:
-            print (conn_err)
-        except requests.exceptions.Timeout as tim_err:
-            print (tim_err)
-        except requests.exceptions.RequestException as req_err:
-            print (req_err)
+        except EOFError as err:
+            print (err)
             
-            
-if __name__ == "__main__":
-    da = Data()
-    #read data from csv file
-    da.read_data_from_csv_file("data.csv")
-    #get list data from csv file
-    print(da.get_list_data())
-    
-    
-    
-            
-            
-        
+da = Data()
+da.read_data_from_csv_file("data.csv")
+
+print(da.get_list_data())
+da.write_data_to_csv_file()
+
+
